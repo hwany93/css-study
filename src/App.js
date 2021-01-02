b2fd8b8b1c47f35c61581ed7b13d644c8873f40f
jakepark@carrotglobal.com <park4023>
2021-01-02 18:35:10 +0900

    add gulf

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ function App() {
     <div className="App">
       auto fit
       <div className="grid auto-fill">
-        <div className="item">1</div>
-        <div className="item">2</div>
-        <div className="item">3</div>
-        <div className="item">4</div>
-        <div className="item">5</div>
-        <div className="item">6</div>
+        <div className="item">1dsadsadasdasd</div>
+        <div className="item">2asdasdasdad</div>
+        <div className="item">3sadasdsadasdasdsdadasdasdas3sadasdsadasdasdsdadasdasdas</div>
+        <div className="item">4dsadasdasdd</div>
+        <div className="item">5dasdasd</div>
+        <div className="item">6dasdasdasdasdasdas</div>
       </div>
 
       auto fill
