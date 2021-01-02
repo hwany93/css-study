c3be61db5b2fa82b99551100d2ad802480e43c87
jakepark@carrotglobal.com <park4023>
2021-01-02 15:48:16 +0900

    grid area

--- src/App.js
+++ src/App.js
@@ -3,18 +3,11 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-      <div className="app-wrapper">
-        <div className="box">1</div>
-        <div className="box">2</div>
-        <div className="box">3</div>
-        <div className="box">4</div>
-        <div className="box">5</div>
-        <div className="box">6</div>
-        <div className="box">7</div>
-        <div className="box">8</div>
-        <div className="box">9</div>
-        <div className="box">10</div>
-        <div className="box">11</div>
+      <div className="grid">
+        <div class="header"></div>
+        <div class="content"></div>
+        <div class="nav"></div>
+        <div class="footer"></div>
       </div>
     </div>
   );
