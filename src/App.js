ef1a8c28a68498b049b7f4ad2312dabf51c9958d
jakepark@carrotglobal.com <park4023>
2021-01-02 15:35:34 +0900

    grid basic

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ function App() {
         <div className="box">1</div>
         <div className="box">2</div>
         <div className="box">3</div>
+        <div className="box">4</div>
+        <div className="box">5</div>
+        <div className="box">6</div>
+        <div className="box">7</div>
+        <div className="box">8</div>
+        <div className="box">9</div>
+        <div className="box">10</div>
+        <div className="box">11</div>
       </div>
     </div>
   );
